84918bbfc37aa78a4f44df86bfd89f9383f5d76c
gustavo <gustavohyppolito@gmail.com>
2015-10-18 19:53:01 +0000

    ajustes

--- src/faster-orm/src/main/java/br/com/ghfsoftware/faster/annotation/Join.java
+++ src/faster-orm/src/main/java/br/com/ghfsoftware/faster/annotation/Join.java
@@ -18,6 +18,38 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 @Documented
 public @interface Join {
-	
+
+    /**
+     * Annotation to indicate the relation between the fields
+     */
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.METHOD)
+    @Documented
+    @interface Relation {
+
+        /**
+         * Column name of the local table
+         * @return name of the column on data base
+         */
+        String name();
+
+        /**
+         * Column name of table related
+         * @return name of the column on data base
+         */
+        String columnRelated();
+
+        /**
+         * Inform if the local field accept null or not
+         * @return not null or null
+         */
+        boolean isNotNull() default true;
+    }
+
+    /**
+     * Mapped the fields related
+     * @return relations list
+     */
+    Relation[] value() default {};
 	
 }
